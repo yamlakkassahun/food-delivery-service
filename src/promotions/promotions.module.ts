43ffee2b61b83ff2beac305f3294e82ef557ca84
Yamlak Kassahun <yamlak.k@gmail.com>
2024-03-26 16:53:03 +0300

    removed the genrated resources

--- src/promotions/promotions.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PromotionsService } from './promotions.service';
-import { PromotionsController } from './promotions.controller';
-
-@Module({
-  controllers: [PromotionsController],
-  providers: [PromotionsService],
-})
-export class PromotionsModule {}
